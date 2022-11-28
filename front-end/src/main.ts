f7a8b270564c3b3085f063e714c717f45d176fd5
ketchup3313 <skw0852@autuni.ac.nz>
2022-11-29 02:29:39 +0800

    1
    trash

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -3,7 +3,9 @@ import App from './App.vue'
 import './common/base.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import router from './router'
+import {createPinia} from 'pinia'
 const app = createApp(App)
+createApp(App).use(createPinia()).mount('#app') 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
