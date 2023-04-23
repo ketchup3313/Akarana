5ee8364d62c537d9f5ce05f2ed31170130fcee94
Hugo <liyijundashuaibi@gmail.com>
2023-04-24 11:14:47 +1200

    Update Photo.vue

--- src/src/views/Photo.vue
+++ src/src/views/Photo.vue
@@ -1,25 +1,20 @@
-<!-- <template>
-  <div class="home">
-    <el-container>
-      <el-container>
-        <el-header>
-          <Header :handleCollapse="handleCollapse" :isCollapse="isCollapse" />
-        </el-header>
-        <el-main>
-          <div class="photo-wall">
-            <div v-for="(image, index) in images" :key="index" class="photo-wall__item">
-              <img :src="image.src" :alt="image.alt">
-            </div>
-          </div>
-        </el-main>
-      </el-container>
-    </el-container>
+<template>
+  <div>
+    <el-carousel :interval="4000" type="card" height="200px">
+      <el-carousel-item v-for="(image, index) in images" :key="index">
+        <img :src="image.src" :alt="image.alt" style="width: 100%; height: 100%; object-fit: cover;">
+      </el-carousel-item>
+    </el-carousel>
+    <div class="photo-wall">
+      <div v-for="(image, index) in images" :key="index" class="photo-wall__item" style="width: 250px; height: 250px;">
+        <img :src="image.src" :alt="image.alt" style="width: 100%; height: 100%; object-fit: cover;">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'PhotoWall',
   data() {
     return {
       images: []
@@ -30,21 +25,13 @@ export default {
   },
   methods: {
     async getImages() {
-      // 从本地加载图片
       const images = [
-        { src: require('@/assets/images/20130330_135412.jpg'), alt: 'Image 1' },
-        { src: require('@/assets/images/20130421_100806.jpg'), alt: 'Image 2' },
-        { src: require('@/assets/images/20131207_182736.jpg'), alt: 'Image 3' },
-        { src: require('@/assets/images/20131207_183958.jpg'), alt: 'Image 4' },
-        { src: require('@/assets/images/20131116_160740.jpg'), alt: 'Image 10' },
-        { src: require('@/assets/images/DSC00108.jpg'), alt: 'Image 5' },
-        { src: require('@/assets/images/20131207_182736.jpg'), alt: 'Image 6' },
-        { src: require('@/assets/images/20131207_183958.jpg'), alt: 'Image 7' },
-        { src: require('@/assets/images/20130421_100806.jpg'), alt: 'Image 8' },
-        { src: require('@/assets/images/20130421_100806.jpg'), alt: 'Image 9' },
+        { src: "http://akarana.oss-ap-southeast-1.aliyuncs.com/car1.jpg", alt: 'Image 1' },
+        { src: "http://akarana.oss-ap-southeast-1.aliyuncs.com/car2.jpg", alt: 'Image 2' },
+        { src: "http://akarana.oss-ap-southeast-1.aliyuncs.com/car3.jpg", alt: 'Image 3' },
+        { src: "http://akarana.oss-ap-southeast-1.aliyuncs.com/car4.jpg", alt: 'Image 4' },
       ]
 
-      // 将本地图片赋值给 images 属性
       this.images = images
     }
   }
@@ -53,26 +40,25 @@ export default {
 
 <style>
 .photo-wall {
-  display: flex;
-  flex-wrap: wrap;
-  margin: -10px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  grid-gap: 10px;
+  margin: 10px;
 }
 
 .photo-wall__item {
-  box-sizing: border-box;
-  /* position:fixed; */
   display: flex;
-  width: 25%;
-  padding: 10px;
+  justify-content: center;
+  align-items: center;
 }
 
 .photo-wall__item img {
-  display: block;
-  width: 100%;
+  max-width: 100%;
+  max-height: 100%;
   transition: transform 0.3s ease-in-out;
 }
 
 .photo-wall__item:hover img {
   transform: scale(1.2);
 }
-</style> -->
+</style>
