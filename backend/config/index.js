358564872723574083a438bd9ab766ea0b2689f8
ketchup3313 <88459969+ketchup3313@users.noreply.github.com>
2023-04-27 11:27:50 +1200

    Delete backend/config directory

--- backend/config/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  jwtKey: 'YYS',
-  ossClientConfig: {
-    region: 'oss-ap-southeast-1',
-    accessKeyId: 'LTAI5tR2CSMZbgyNBhrgcKn5',
-    accessKeySecret: 'iTa4d64JFXJBt5A4eGJ6OldmTeQQrl',
-    bucket: 'akarana',
-  },
-}
