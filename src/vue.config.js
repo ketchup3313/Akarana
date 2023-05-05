abfe2773883a7a268567d9bee321e51887db785f
ketchup3313 <skw0852@autuni.ac.nz>
2023-05-06 03:30:39 +1200

    Update vue.config.js

--- src/vue.config.js
+++ src/vue.config.js
@@ -13,7 +13,7 @@ module.exports = {
     devServer: {
       proxy: {
         "/api": {//捕获API的标志，如果API中有这个字符串，那么就开始匹配代理，
-          target: "https://akarana-backend.onrender.com/",//代理的api地址，就是要跨域的地址
+          target: "https://akarana-backend.onrender.com",//代理的api地址，就是要跨域的地址
           changeOrigin: true,// 这个参数可以让target参数是域名
           ws: true,//是否启用websockets，用不到可设为false
         },
