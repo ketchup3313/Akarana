128432f47cf12fec66ec05518201dea93d985dec
ketchup3313 <skw0852@autuni.ac.nz>
2023-05-06 04:56:37 +1200

    Update request.js

--- src/src/utils/request.js
+++ src/src/utils/request.js
@@ -6,7 +6,7 @@ import router from "../router/index";
 // 创建axios实例  
 const service = axios.create({
   timeout: 15000, // 请求超时时间
-  headers: {"contenttype": "application/x-www-form-urlencoded"}
+  headers: {"content-type": "application/x-www-form-urlencoded"}
   // baseURL: "https://akarana-backend.onrender.com"
 });
 service.interceptors.request.use((config) => {
