343f4cd2482a940fc80be5aea127ec29bea331a4
ketchup3313 <skw0852@autuni.ac.nz>
2023-05-06 03:18:34 +1200

    1

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,18 +1,18 @@
-// module.exports = {
-//     jwtKey: 'YYS',
-//     ossClientConfig: {
-//       region: 'oss-ap-southeast-1',
-//       accessKeyId: 'LTAI5tR2CSMZbgyNBhrgcKn5',
-//       accessKeySecret: 'iTa4d64JFXJBt5A4eGJ6OldmTeQQrl',
-//       bucket: 'akarana',
-//     },
-//   }
-  module.exports = {
-    jwtKey: process.env.JWT_KEY,
+module.exports = {
+    jwtKey: 'YYS',
     ossClientConfig: {
-      region: process.env.OSS_REGION,
-      accessKeyId: process.env.OSS_ACCESS_KEY_ID,
-      accessKeySecret: process.env.OSS_ACCESS_KEY_SECRET,
-      bucket: process.env.OSS_BUCKET,
+      region: 'oss-ap-southeast-1',
+      accessKeyId: 'LTAI5tR2CSMZbgyNBhrgcKn5',
+      accessKeySecret: 'iTa4d64JFXJBt5A4eGJ6OldmTeQQrl',
+      bucket: 'akarana',
     },
-  };
+  }
+  // module.exports = {
+  //   jwtKey: process.env.JWT_KEY,
+  //   ossClientConfig: {
+  //     region: process.env.OSS_REGION,
+  //     accessKeyId: process.env.OSS_ACCESS_KEY_ID,
+  //     accessKeySecret: process.env.OSS_ACCESS_KEY_SECRET,
+  //     bucket: process.env.OSS_BUCKET,
+  //   },
+  // };
