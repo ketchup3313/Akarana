a42d4deb18efd6e8481828199345bad19df08712
ketchup3313 <88459969+ketchup3313@users.noreply.github.com>
2023-05-08 18:47:45 +1200

    Update index.js

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,9 +1,9 @@
 module.exports = {
-  jwtKey: "YYS",
+  jwtKey: process.env.JWT_KEY,
   ossClientConfig: {
-    region: "oss-ap-southeast-1",
-    accessKeyId: "LTAI5tR2CSMZbgyNBhrgcKn5",
-    accessKeySecret: "iTa4d64JFXJBt5A4eGJ6OldmTeQQrl",
-    bucket: "akarana",
+    region: process.env.OSS_REGION,
+    accessKeyId: process.env.OSS_ACCESS_KEY_ID,
+    accessKeySecret: process.env.OSS_ACCESS_KEY_SECRET,
+    bucket: process.env.OSS_BUCKET,
   },
 };
